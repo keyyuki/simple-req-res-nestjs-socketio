224f3f2a3e32221b3ff9b34a2f966ca86e3f9d73
kiennn01062 <keyyuki@gmail.com>
2022-07-15 13:11:20 +0700

    fix add boostrap to provider list

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { SocketIoListener } from './socket-io.listener';
 import { SocketIoClientProvider } from './socket-io-client.provider';
 import { SocketIoClientProxyService } from './socket-io-client-proxy/socket-io-client-proxy.service';
 import { RequestStoreProvider } from './request-store.provider';
+import { Boostrap } from './boostrap';
 
 @Module({
   imports: [ConfigModule.forRoot()],
@@ -15,6 +16,7 @@ import { RequestStoreProvider } from './request-store.provider';
     SocketIoClientProvider,
     SocketIoClientProxyService,
     RequestStoreProvider,
+    Boostrap,
   ],
 })
 export class AppModule {}
